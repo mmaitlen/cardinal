ddc723cc173ddc62bd98436cebca573c48359fa2
Michael Maitlen <michael.maitlen@uncorkedstudios.com>
2016-12-30 10:39:24 -0800

    use const keyword, decouple variable name from Arduino pin

--- cardinal_intro/cardinal_intro.ino
+++ cardinal_intro/cardinal_intro.ino
@@ -1,36 +1,36 @@
 // define variables for each led
-int LED_13 = 13;
-int LED_12 = 12;
-int LED_11 = 11;
-int LED_10 = 10;
-int LED_9 = 9;
-int LED_8 = 8;
+const int LED1 = 13;
+const int LED2 = 12;
+const int LED3 = 11;
+const int LED4 = 10;
+const int LED5 = 9;
+const int LED6 = 8;
 
 void setup() {
   // setup pins as outputs for leds
-  pinMode(LED_13, OUTPUT);
-  pinMode(LED_12, OUTPUT);
-  pinMode(LED_11, OUTPUT);
-  pinMode(LED_10, OUTPUT);
-  pinMode(LED_9, OUTPUT);
-  pinMode(LED_8, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
 }
 
 void loop() {
   // turn leds on
-  digitalWrite(LED_13, HIGH);
-  digitalWrite(LED_12, HIGH);
-  digitalWrite(LED_11, HIGH);
-  digitalWrite(LED_10, HIGH);
-  digitalWrite(LED_9, HIGH);
-  digitalWrite(LED_8, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  digitalWrite(LED5, HIGH);
+  digitalWrite(LED6, HIGH);
   delay(1000);            
   // turn leds off    
-  digitalWrite(LED_13, LOW);
-  digitalWrite(LED_12, LOW);
-  digitalWrite(LED_11, LOW);
-  digitalWrite(LED_10, LOW);
-  digitalWrite(LED_9, LOW);
-  digitalWrite(LED_8, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
   delay(1000);                
 }
