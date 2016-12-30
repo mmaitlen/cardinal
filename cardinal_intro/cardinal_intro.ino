30c3dc67586540722be057abe8f17d349d828e3b
Michael Maitlen <michael.maitlen@uncorkedstudios.com>
2016-12-30 10:24:23 -0800

    define leds using arduino pins

--- cardinal_intro/cardinal_intro.ino
+++ cardinal_intro/cardinal_intro.ino
@@ -1,33 +1,28 @@
-
-int LED_13 = 13;
-int LED_12 = 12;
-int LED_11 = 11;
-int LED_10 = 10;
-int LED_9 = 9;
-int LED_8 = 8;
-
 void setup() {
-  pinMode(LED_13, OUTPUT);
-  pinMode(LED_12, OUTPUT);
-  pinMode(LED_11, OUTPUT);
-  pinMode(LED_10, OUTPUT);
-  pinMode(LED_9, OUTPUT);
-  pinMode(LED_8, OUTPUT);
+  // setup pins as outputs for leds
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_13, HIGH);
-  digitalWrite(LED_12, HIGH);
-  digitalWrite(LED_11, HIGH);
-  digitalWrite(LED_10, HIGH);
-  digitalWrite(LED_9, HIGH);
-  digitalWrite(LED_8, HIGH);
-  delay(1000);                
-  digitalWrite(LED_13, LOW);
-  digitalWrite(LED_12, LOW);
-  digitalWrite(LED_11, LOW);
-  digitalWrite(LED_10, LOW);
-  digitalWrite(LED_9, LOW);
-  digitalWrite(LED_8, LOW);
+  // turn leds on
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  delay(1000);       
+  // turn leds off         
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
   delay(1000);                
 }
