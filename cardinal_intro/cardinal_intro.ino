4caec325cfe2708f0f5506dae57f0819daa9addb
Michael Maitlen <michael.maitlen@uncorkedstudios.com>
2016-12-30 10:53:09 -0800

    intro arrays and for loops

--- cardinal_intro/cardinal_intro.ino
+++ cardinal_intro/cardinal_intro.ino
@@ -6,31 +6,25 @@ const int LED4 = 10;
 const int LED5 = 9;
 const int LED6 = 8;
 
+const int LEDS[] = {LED1, LED2, LED3, LED4, LED5, LED6};
+
 void setup() {
   // setup pins as outputs for leds
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
+  for (int idx = 0; idx <= 5; idx++) {
+    pinMode(LEDS[idx], OUTPUT);
+  }
 }
 
 void loop() {
   // turn leds on
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  digitalWrite(LED5, HIGH);
-  digitalWrite(LED6, HIGH);
+  for (int idx = 0; idx <= 5; idx++) {
+    digitalWrite(LEDS[idx], HIGH);
+  }
   delay(1000);            
-  // turn leds off    
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
-  digitalWrite(LED4, LOW);
-  digitalWrite(LED5, LOW);
-  digitalWrite(LED6, LOW);
+  
+  // turn leds off 
+  for (int idx = 0; idx <= 5; idx++) {
+    digitalWrite(LEDS[idx], LOW);
+  }
   delay(1000);                
 }
